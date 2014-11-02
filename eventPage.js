eae558f0ad3ae6a8ecea264d85047e7c1dbfedf5
helenqu <helenqu@seas.upenn.edu>
2014-11-02 05:44:58 -0500

    ALMOST THERE

--- eventPage.js
+++ eventPage.js
@@ -1,48 +1,65 @@
-$(function(){
 
-var yoColors = ['#86BE95', '#FE51D5', '#5E2ECC', '#00F6AD', '#AFB109', '#18A2E9', '#CB1136', '#67D3D6', '#3BCE58', '#5E76D0'];
 
-var buddyList = JSON.parse(localStorage.getItem('buddies'));
-  var shuffledColors = _.shuffle(yoColors);
-  if (buddyList){
-    for (var i = 0; i < buddyList.length; i++){
-      var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(buddyList[i])));
-      $buddy.css('background-color', shuffledColors[i % buddyList.length]);
-      $('.buddy-list').append($buddy);
-    }
-  }
 
-$('#yoform').submit(function(event){
-    event.preventDefault();
-    var username = $('#username').val();
+$(function() {
 
-    chrome.storage.local.get(['apiKey'], function(data) {
-      var request = require('request');
-      request.post('http://api.justyo.co/yo/',
-      { form: { 'api_token': data.apiKey,
-              'username': $('#username').val(),
-              'link': alert(document.URL) } },
-      function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        }
-      }
-    }
-  });
-    
-    var buddyList = JSON.parse(localStorage.getItem('buddies'));
-    if (!buddyList) { buddyList = []; }
-    var buddyIndex = buddyList.indexOf(username);
-    if (buddyIndex === -1){
-      buddyList.unshift(username);
-      localStorage.setItem('buddies', JSON.stringify(buddyList));
-      var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(username)));
-      $buddy.css('background-color', yoColors[Math.floor(Math.random() * 10)]);
-      $('.buddy-list').prepend($buddy);
-    } else if (buddyIndex >= 0){
-      // move buddy to the front of the array
-      buddyList.splice(buddyIndex, 1);
-      buddyList.unshift(username);
-      localStorage.setItem('buddies', JSON.stringify(buddyList));
-    }
-  });
+            var yoColors = ['#86BE95', '#FE51D5', '#5E2ECC', '#00F6AD', '#AFB109', '#18A2E9', '#CB1136', '#67D3D6', '#3BCE58', '#5E76D0'];
+            var url=null;
+            var buddyList = JSON.parse(localStorage.getItem('buddies'));
+            var shuffledColors = _.shuffle(yoColors);
+            if (buddyList) {
+                for (var i = 0; i < buddyList.length; i++) {
+                    var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(buddyList[i])));
+                    $buddy.css('background-color', shuffledColors[i % buddyList.length]);
+                    $('.buddy-list').append($buddy);
+                }
+            }
+
+            chrome.tabs.getSelected(null,function(tab){
+                url=tab.url;//tab.id.url;
+            });
+
+
+            $('#yoform').submit(function(event) {
+                event.preventDefault();
+                var username = $('#username').val();
+                var api_token =  '137f1ca55799404dd51da56246b62f5747e58437'//\\JSON.parse(localStorage.getItem('apiKey'));
+                /*chrome.storage.sync.get(['apiKey'], function(data) {
+                    var opts = {
+                        api_token: data.apiKey,
+                        username: useame,
+                        link: $(location).attr('href')
+                    };*/
+                    // http://api.justyo.co/yo/
+                var opts = {
+                        api_token: api_token,
+                        username: username,
+                        link: url//$(location).attr('href')
+                    };
+                    $.post('http://api.justyo.co/yo/', opts, function(output) {
+                        console.log(output);
+                        $('#username').val('');
+                        $('.err-msg').hide();
+                        }).fail(function(){
+                        $('.err-msg').show();
+                    });
+                });
+
+                    var buddyList = JSON.parse(localStorage.getItem('buddies'));
+                    if (!buddyList) {
+                        buddyList = [];
+                    }
+                    var buddyIndex = buddyList.indexOf(username);
+                    if (buddyIndex === -1) {
+                        buddyList.unshift(username);
+                        localStorage.setItem('buddies', JSON.stringify(buddyList));
+                        var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(username)));
+                        $buddy.css('background-color', yoColors[Math.floor(Math.random() * 10)]);
+                        $('.buddy-list').prepend($buddy);
+                    } else if (buddyIndex >= 0) {
+                        // move buddy to the front of the array
+                        buddyList.splice(buddyIndex, 1);
+                        buddyList.unshift(username);
+                        localStorage.setItem('buddies', JSON.stringify(buddyList));
+                    }
+                });
--- opening.js
+++ opening.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   $('#introform').on('submit', function(event) {
     event.preventDefault();
-    var YOSERNAME = $('#username').val();
-    var YOPIKEY = $('#apikey').val();
+    /*var YOSERNAME = $('#username').val();
+    var YOPIKEY = $('#apikey').val(); */
     console.log(YOSERNAME);
-    chrome.storage.local.set({
-      userName: YOSERNAME,
-      apiKey: YOPIKEY
-     }
-    }
-  });
-  chrome.storage.local.get(['apiKey', 'userName'], function(data) {
+    localStorage.setItem('userName',$('#userName').val());
+    localStorage.setItem('apiKey',$('#apiKey').val());
+    //localStorage.setItem('apiKey','HELLOWORLD');
+    /*chrome.storage.sync.set({
+      'userName': YOSERNAME,
+      'apiKey': YOPIKEY
+     }); */
+  /* chrome.storage.sync.get(['apiKey', 'userName'], function(data) {
     if (data.apiKey > '' || data.userName > '') {
       $('#YOPIKEY').val(data.apiKey);
       $('#username').val(data.userName);
     }
-  });
+  }); */
+});
