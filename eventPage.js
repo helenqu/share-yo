417f68d1a035a64c8dba17adbc2eb31d1e20936f
helenqu <helenqu@seas.upenn.edu>
2014-11-01 21:44:26 -0400

    Added eventPage.js

---
+++ eventPage.js
@@ -0,0 +1,16 @@
+var http = new XMLHttpRequest();
+var url = "https://api.justyo.co/yoall/";
+var params = "api_token=<token>&link=http://optional-link.com";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.setRequestHeader("Content-length", params.length);
+http.setRequestHeader("Connection", "close");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
