e347317c5191b729b8253b29d57c0df06e03726a
gearsgrinding <hualex@seas.upenn.edu>
2014-11-02 22:39:55 -0500

    fixed JSON error

--- eventPage.js
+++ eventPage.js
@@ -5,7 +5,8 @@ $(function() {
 
             var yoColors = ['#86BE95', '#FE51D5', '#5E2ECC', '#00F6AD', '#AFB109', '#18A2E9', '#CB1136', '#67D3D6', '#3BCE58', '#5E76D0'];
             var url=null;
-            var buddyList = JSON.parse(localStorage.getItem('buddies'));
+            var buddyList = [];
+            //var buddyList = JSON.parse(localStorage.getItem('buddies'));
             var shuffledColors = _.shuffle(yoColors);
             if (buddyList) {
                 for (var i = 0; i < buddyList.length; i++) {
@@ -43,15 +44,19 @@ $(function() {
                         }).fail(function(){
                         $('.err-msg').show();
                     });
-                });
+                
 
-                    var buddyList = JSON.parse(localStorage.getItem('buddies'));
+                    //var buddyList = JSON.parse(localStorage.getItem('buddies'));
+                    console.log(buddyList);
+                    console.log(buddyList[0]);
+                    
                     if (!buddyList) {
                         buddyList = [];
                     }
                     var buddyIndex = buddyList.indexOf(username);
                     if (buddyIndex === -1) {
                         buddyList.unshift(username);
+
                         localStorage.setItem('buddies', JSON.stringify(buddyList));
                         var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(username)));
                         $buddy.css('background-color', yoColors[Math.floor(Math.random() * 10)]);
@@ -63,3 +68,4 @@ $(function() {
                         localStorage.setItem('buddies', JSON.stringify(buddyList));
                     }
                 });
+});
