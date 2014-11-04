ce61bd57e2dcd12db047fb5e6b958889a7bc45ad
gearsgrinding <hualex@seas.upenn.edu>
2014-11-04 18:02:03 -0500

    fixing buddy list functionality

--- eventPage.js
+++ eventPage.js
@@ -1,42 +1,31 @@
-
-
-
 $(function() {
 
             var yoColors = ['#86BE95', '#FE51D5', '#5E2ECC', '#00F6AD', '#AFB109', '#18A2E9', '#CB1136', '#67D3D6', '#3BCE58', '#5E76D0'];
             var url=null;
             var buddyList = [];
-            //var buddyList = JSON.parse(localStorage.getItem('buddies'));
             var shuffledColors = _.shuffle(yoColors);
-            if (buddyList) {
-                for (var i = 0; i < buddyList.length; i++) {
-                    var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(buddyList[i])));
-                    $buddy.css('background-color', shuffledColors[i % buddyList.length]);
-                    $('.buddy-list').append($buddy);
+            var buddyList = JSON.parse(localStorage.getItem('buddies'));
+            
+            //Displays the buddy list upon opening
+            if (buddyList){
+                for (var i = 0; i < buddyList.length; i++){
+                  var $buddy = $('<tr>').append($('<td>').append($('<a>').attr('href', '#').text(buddyList[i])));
+                  $buddy.css('background-color', shuffledColors[i % buddyList.length]);
+                  $('.buddy-list').append($buddy);
+                 }
                 }
-            }
 
-            chrome.tabs.getSelected(null,function(tab){
-                url=tab.url;//tab.id.url;
-            });
-
-
+                //funciton that receives the form data, and activates upon submit.
             $('#yoform').submit(function(event) {
                 event.preventDefault();
                 var username = $('#username').val();
-                var api_token = (localStorage.getItem('apiKey')); //'137f1ca55799404dd51da56246b62f5747e58437'
-                /*chrome.storage.sync.get(['apiKey'], function(data) {
-                    var opts = {
-                        api_token: data.apiKey,
-                        username: useame,
-                        link: $(location).attr('href')
-                    };*/
-                    // http://api.justyo.co/yo/
+                var api_token = (localStorage.getItem('apiKey')); 
                 var opts = {
                         api_token: api_token,
                         username: username,
-                        link: url//$(location).attr('href')//url
+                        link: url
                     };
+                    //using api to send the data.
                     $.post('http://api.justyo.co/yo/', opts, function(output) {
                         console.log(output);
                         $('#username').val('');
@@ -44,15 +33,7 @@ $(function() {
                         }).fail(function(){
                         $('.err-msg').show();
                     });
-                
-
-                    //var buddyList = JSON.parse(localStorage.getItem('buddies'));
-                    console.log(buddyList);
-                    console.log(buddyList[0]);
                     
-                    if (!buddyList) {
-                        buddyList = [];
-                    }
                     var buddyIndex = buddyList.indexOf(username);
                     if (buddyIndex === -1) {
                         buddyList.unshift(username);
@@ -67,5 +48,11 @@ $(function() {
                         buddyList.unshift(username);
                         localStorage.setItem('buddies', JSON.stringify(buddyList));
                     }
+                });
+                        //Autofills the username by clicking buddy list
+                    $('.buddy-list a').on('click', function(e){
+                     e.preventDefault();
+                     var buddyName = $(this).text();
+                    $('#username').val(buddyName);
                 });
 });
